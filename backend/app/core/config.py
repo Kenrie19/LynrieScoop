712213e5ac02e94710ba81177da8953a40bbddef
SimonStnn <simon.stijnen.23@gmail.com>
2025-05-16 14:07:25 +0200

    Correctly load env in backend

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,6 +9,9 @@ from pydantic import (
     field_validator,
 )
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
@@ -72,7 +75,9 @@ class Settings(BaseSettings):
 settings = Settings(
     DATABASE_URL=os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/cinema"),  # type: ignore
     JWT_SECRET=os.getenv("JWT_SECRET", "NOT_A_SECRET"),
-    TMDB_API_KEY="995ebcf9d5249c3ccdbd4b689c4b5a3f",
+    TMDB_API_KEY=os.getenv("TMDB_API_KEY", "NOT_A_SECRET"),
 )
 
-print("Settings loaded:", settings.TMDB_API_KEY)  # Debugging line to check loaded settings
+print(
+    "Settings loaded:", settings.TMDB_API_KEY
+)  # Debugging line to check loaded settings
